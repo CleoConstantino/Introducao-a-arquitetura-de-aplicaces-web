0c4d08f25f6da537f9b198c2ab66bb2dfa37e2dd
Cleo Constantino <cleoconstantino@outlook.com>
2022-07-18 14:46:45 -0300

    Modificamos o mongo.js para exportar a conexao e coletion e modificamos o server.js para responder com o conteudo de um documento de um banco de dados

--- server/mongo.js
+++ server/mongo.js
@@ -2,16 +2,17 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://database_user:iuGBR83GFk3eGGLY@firstcluster.ynxjxrm.mongodb.net/?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri);
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  collection.findOne().then(
-    (item) => {
-      console.log(item.greeting)
-    },
-    () => {
-      client.close()
-    }
-  )  
-});
+var collection;
+
+module.exports = {
+  connect: () => {
+    client.connect((_) => {
+      collection = client.db("test").collection("devices");
+    });
+  },
+  collection: () => {
+    return collection;
+  },
+};
--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const mongo = require('./mongo');
+
 const server = express();
 server.use(cors());
 
 server.get('/', (_,res) => {
-    res.json('Hello Express');
+    mongo
+        .collection()
+        .findOne({ greeting: 'Hello Mongo'})
+        .then((doc) => res.json(`${doc.greeting} + Express`));
 });
 
-server.listen(3001, '127.0.0.1');
+server.listen(3001, () => {
+    console.log('Server running on port 3001');
+    mongo.connect();
+});
