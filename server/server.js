b788a37569b924c4fe42cd5d36d72d22109901c2
Cleo Constantino <cleoconstantino@outlook.com>
2022-07-18 09:52:18 -0300

    Exutamos o npm install cors e adicionamos o cors ao express

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const server = express();
+server.use(cors());
 
 server.get('/', (_,res) => {
-    res.end('Hello Express');
+    res.json('Hello Express');
 });
 
 server.listen(3001, '127.0.0.1');
